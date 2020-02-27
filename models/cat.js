12ca397ccc2e80f45911ea78b46522cbb0b03cb2
Leticia Guerra <g.leticia@rocketmail.com>
2020-02-27 00:30:59 -0500

    updating vars

--- models/cat.js
+++ models/cat.js
@@ -1,13 +1,13 @@
 // Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
+const orm = require("../config/orm.js");
 
-var cat = {
+const cat = {
   all: function(cb) {
     orm.all("cats", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+  // The constiables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create("cats", cols, vals, function(res) {
       cb(res);
