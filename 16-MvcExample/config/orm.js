ac3d108ffa89f521cb3ad4729765c762e083d2f7
Leticia Guerra <g.leticia@rocketmail.com>
2020-02-27 22:38:56 -0500

    updatign working file

--- 16-MvcExample/config/orm.js
+++ 16-MvcExample/config/orm.js
@@ -1,5 +1,5 @@
 // Import MySQL connection.
-const connection = require("../config/connection.js");
+var connection = require("../config/connection.js");
 
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
@@ -7,9 +7,9 @@ const connection = require("../config/connection.js");
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
-  const arr = [];
+  var arr = [];
 
-  for (const i = 0; i < num; i++) {
+  for (var i = 0; i < num; i++) {
     arr.push("?");
   }
 
@@ -18,11 +18,11 @@ function printQuestionMarks(num) {
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
-  const arr = [];
+  var arr = [];
 
   // loop through the keys and push the key/value as a string int arr
-  for (const key in ob) {
-    const value = ob[key];
+  for (var key in ob) {
+    var value = ob[key];
     // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
@@ -30,7 +30,7 @@ function objToSql(ob) {
         value = "'" + value + "'";
       }
       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {hungry: true} => ["hungry=true"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -40,9 +40,9 @@ function objToSql(ob) {
 }
 
 // Object for all our SQL statement functions.
-const orm = {
+var orm = {
   all: function(tableInput, cb) {
-    const queryString = "SELECT * FROM " + tableInput + ";";
+    var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
@@ -51,7 +51,7 @@ const orm = {
     });
   },
   create: function(table, cols, vals, cb) {
-    const queryString = "INSERT INTO " + table;
+    var queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
@@ -70,9 +70,9 @@ const orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, hungry: true}
+  // An example of objColVals would be {name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
-    const queryString = "UPDATE " + table;
+    var queryString = "UPDATE " + table;
 
     queryString += " SET ";
     queryString += objToSql(objColVals);
@@ -87,21 +87,8 @@ const orm = {
 
       cb(result);
     });
-  },
-  delete: function(table, condition, cb) {
-    const queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
   }
 };
 
-// Export the orm object for the model (burger.js).
+// Export the orm object for the model (cat.js).
 module.exports = orm;
--- 16-MvcExample/models/cat.js
+++ 16-MvcExample/models/cat.js
@@ -1,13 +1,13 @@
 // Import the ORM to create functions that will interact with the database.
-const orm = require("../config/orm.js");
+var orm = require("../config/orm.js");
 
-const burger = {
+var cat = {
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The constiables cols and vals are arrays.
+  // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
@@ -17,13 +17,8 @@ const burger = {
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
   }
 };
 
-// Export the database functions for the controller (catsController.js).
-module.exports = burger;
+// Export the database functions for the controller (burgersController.js).
+module.exports = cat;
