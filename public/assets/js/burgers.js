12c6c1b61bf2d5d54d7fca66d7d1599f4e74b196
Leticia Guerra <g.leticia@rocketmail.com>
2020-02-29 16:23:50 -0500

    finished

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
+
 $(function() {
-  $(".change-hung").on("click", function(event) {
+  $("#change-hung").on("click", function(event) {
     var id = $(this).data("id");
     var newhung = $(this).data("newhung");
 
@@ -8,21 +8,21 @@ $(function() {
       eaten: newhung
     };
 
-    // Send the PUT request.
+
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newhungState
     }).then(
       function() {
         console.log("changed hung to", newhung);
-        // Reload the page to get the updated list
+
         location.reload();
       }
     );
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
+
     event.preventDefault();
 
     var newBurger = {
@@ -30,14 +30,13 @@ $(function() {
       eaten: $("[name=eaten]").val().trim()
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
+      
         location.reload();
       }
     );
